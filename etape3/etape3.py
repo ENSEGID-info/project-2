333e9eaa1e63522f6b47118dfd3971ee404e26fa
JulienErnenwein <julien.ernenwein2@gmail.com>
2025-11-26 17:23:20 +0100

    lissage de la pente

--- etape3/etape3.py
+++ etape3/etape3.py
@@ -1,9 +1,20 @@
-from numpy import*
+from numpy import *
+import math as m
+
+e=[4,5,6,7,8,9,4,6,8,2]
+a=[12,11.5,10.5,10,9,8.5,8,7.5,7,6.5]
 
 def epaisseur_fact(n,alpha):
-    return (sin(alpha)**(n/(n+2)))
-    
-    print("Exécution de l'étape 3...")
-    # Exemple : display of the result
-    print("Maximum is " + str(result) + ".")
+
+    return pow(m.sin(m.radians(alpha)),(n/(n+2)))
+
+
+def etape3_main(n):
+    r=[]
+    for k in range(len(e)):
+        t=e[k] * epaisseur_fact(n,a[k])
+        r.append(t)
+
+    return r
+
 
