5800d7f5e72705f51c15e00f46cad7a1a90eb0ca
Louise <kabira8@proton.me>
2025-11-26 17:09:32 +0100

    Ajout d'un script de notes pour l'étape d'interpolation.

---
+++ etape2/Notes_interpolation.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 26 17:07:31 2025
+
+@author: lalloulamarc
+"""
+### Un brouillon de notes
+'''The ice thickness calculated in step (4) is used as input
+for a spatial interpolation. The glacier outline is used as
+a boundary condition with zero ice thickness. The inter-
+polation routine uses an inverse distance averaging tech-
+nique, weighting the individual interpolation nodes with
+the inverse of the squared distance from the considered
+point.'''
