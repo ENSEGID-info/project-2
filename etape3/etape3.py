4d2ce0bb31cc14644f03654574dd464376c442d4
Souadou06 <souadou.romanediouf@gmail.com>
2025-11-26 16:27:12 +0100

    ajout d'épaisseur

--- etape3/etape3.py
+++ etape3/etape3.py
@@ -1,4 +1,8 @@
-def etape3_main(result):
+from numpy import*
+
+def epaisseur_fact(n,alpha):
+    return (sin(alpha)**(n/(n+2)))
+    
     print("Exécution de l'étape 3...")
     # Exemple : display of the result
     print("Maximum is " + str(result) + ".")
