2df985828d99c9221db190667d5b0d30cdf7d947
souadou <souadou.romanediouf@gmail.com>
2025-12-05 09:20:57 +0100

    prise en compte de l'effet local  à la pente fini
    fini

--- etape3/etape3.py
+++ etape3/etape3.py
@@ -1,7 +1,7 @@
 from numpy import *
 import math as m
 
-e=[4,5,6,7,8,9,4,6,8,2]
+
 a=[12,11.5,10.5,10,9,8.5,8,7.5,7,6.5]
 
 def epaisseur_fact(n,alpha):
@@ -9,7 +9,7 @@ def epaisseur_fact(n,alpha):
     return pow(m.sin(m.radians(alpha)),(n/(n+2)))
 
 
-def etape3_main(n):
+def etape3_main(n,e):
     r=[]
     for k in range(len(e)):
         t=e[k] * epaisseur_fact(n,a[k])
@@ -17,4 +17,11 @@ def etape3_main(n):
 
     return r
 
+def etape3(e):
+    ef=[]
+    l=etape3_main(3,e)
+    for k in range(len(e)):
+        i=e[k]-l[k]
+        ef.append(i)
+    return ef
 
